41ffb41ff3892e6d1a955053edba42d1bfa76790
abhishekra07 <abhira719@gmail.com>
2023-05-10 14:42:36 +0530

    Update ProductResource.java

--- src/main/java/com/codelabs/resources/ProductResource.java
+++ src/main/java/com/codelabs/resources/ProductResource.java
@@ -1,8 +1,28 @@
 package com.codelabs.resources;
 
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/v1/product")
 public class ProductResource {
 
+    List<Product> products = new ArrayList<>();
+
+    /**
+     * GET API to return list of products
+     * @return
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProducts() {
+        return Response.ok(products).build();
+    }
+
+    
 }
